aef70102cff05964ffb16c9948b98f14b5cdd99e
Edwin <134835@ISD-PF0XCXKT.gdcusaadc.com>
2020-07-14 23:46:57 +0800

    fix url issue in eks

--- innov8-interviewer-ui/src/redux/constant/service-urls.js
+++ innov8-interviewer-ui/src/redux/constant/service-urls.js
@@ -1,12 +1,12 @@
 // Interviewer api
-export const INTERVIEWER_GET_API_URL = 'api/v1/interviewer';
+export const INTERVIEWER_GET_API_URL = 'api/v1/interviewer/';
 export const INTERVIEWER_GET_SKILL_API_URL = 'api/v1/interviewer/?skill=';
-export const INTERVIEWER_POST_API_URL = 'api/v1/interviewer';
+export const INTERVIEWER_POST_API_URL = 'api/v1/interviewer/';
 export const INTERVIEWER_PUT_API_URL = 'api/v1/interviewer/{id}';
 export const INTERVIEWER_DELETE_API_URL = 'api/v1/interviewer/{id}';
 
 // Requirement api
-export const REQUIREMENT_GET_API_URL = 'api/v1/requirement';
+export const REQUIREMENT_GET_API_URL = 'api/v1/requirement/';
 
 // Contact api
-export const CONTACT_GET_API_URL = 'api/v1/contact';
+export const CONTACT_GET_API_URL = 'api/v1/contact/';
